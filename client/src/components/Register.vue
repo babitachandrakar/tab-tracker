333e0064b62f97321189eb5b7702c7b52fc4c27e
babita chandrakar <babita@babita.c>
2017-11-02 15:52:19 +0530

    remote response from register vue page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -21,11 +21,10 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
+      await AuthenticationService.register({
         email: this.email,
         password: this.password
       })
-      console.log(response.data)
     }
   }
 
